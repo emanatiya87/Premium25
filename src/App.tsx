0224282cd3d3d73a26da4c9bb7e0c1f0ec53d385
MahMoudMostaAfa <mahmoud2030m@gmail.com>
2025-04-16 08:42:41 +0200

    add lazy loading to reduce chunck sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import AppLayout from "./ui/AppLayout";
-import UploadCv from "./pages/UploadCv";
-import PremiumForm from "./pages/PremiumForm";
-import Interview from "./pages/Interview";
-import Quiz from "./pages/Quiz";
-import Data from "./pages/Data";
-import NotFound from "./pages/NotFound";
+// import AppLayout from "./ui/AppLayout";
+// import PremiumForm from "./pages/PremiumForm";
+// import Interview from "./pages/Interview";
+// import UploadCv from "./pages/UploadCv";
+// import Quiz from "./pages/Quiz";
+// import Data from "./pages/Data";
+// import NotFound from "./pages/NotFound";
+// import Success from "./pages/Success";
+// import ChangeStatus from "./pages/ChangeStatus";
+// import AddInterview from "./pages/AddInterview";
+import ThankYou from "./pages/ThankYou";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-import Success from "./pages/Success";
-import ThankYou from "./pages/ThankYou";
-import AddInterview from "./pages/AddInterview";
-import ChangeStatus from "./pages/ChangeStatus";
+import { lazy, Suspense } from "react";
+import PageSpinner from "./ui/PageSpinner";
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Data = lazy(() => import("./pages/Data"));
+const Interview = lazy(() => import("./pages/Interview"));
+const PremiumForm = lazy(() => import("./pages/PremiumForm"));
+const UploadCv = lazy(() => import("./pages/UploadCv"));
+const Quiz = lazy(() => import("./pages/Quiz"));
+const Success = lazy(() => import("./pages/Success"));
+const AddInterview = lazy(() => import("./pages/AddInterview"));
+const ChangeStatus = lazy(() => import("./pages/ChangeStatus"));
 
 function App() {
   const queryClient = new QueryClient({
@@ -26,20 +38,22 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter basename="APEC-Premium-2025">
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<PremiumForm />} />
-            <Route path="/uploadcv" element={<UploadCv />} />
-            <Route path="/interviewslot" element={<Interview />} />
-            <Route path="/add-interview" element={<AddInterview />} />
-            <Route path="/change-status" element={<ChangeStatus />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/ThankYou" element={<ThankYou />} />
-            <Route path="/success" element={<Success />} />
-          </Route>
-          <Route path="/admin25" element={<Data />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<PageSpinner />}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<PremiumForm />} />
+              <Route path="/uploadcv" element={<UploadCv />} />
+              <Route path="/interviewslot" element={<Interview />} />
+              <Route path="/add-interview" element={<AddInterview />} />
+              <Route path="/change-status" element={<ChangeStatus />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/ThankYou" element={<ThankYou />} />
+              <Route path="/success" element={<Success />} />
+            </Route>
+            <Route path="/admin25" element={<Data />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <Toaster position="top-center" reverseOrder={false} />
     </QueryClientProvider>
---
+++ src/ui/PageSpinner.tsx
@@ -0,0 +1,12 @@
+function PageSpinner() {
+  return (
+    <div className=" flex justify-center items-center h-screen">
+      <img
+        src="https://apeceg.com/APEC-Premium-2025/apecLogo.png"
+        className="f-spinner w-20"
+      />
+    </div>
+  );
+}
+
+export default PageSpinner;
